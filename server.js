487b9d056c3b2f861beed0959c79dca9e256e1cc
M Hasnain Khalid <ch.hasnainkhalid@gmail.com>
2019-07-30 23:45:29 +0500

    Working Weather API

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const bodyParser    = require('body-parser');
+const request       = require('request');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded( { extended: true} ));
+
+app.get('/', function (req, res) {
+  //res.send('Hello Hasnain!')
+  res.render('index', {weather: null, error: null });
+});
+
+app.post('/', function(req, res) {
+    
+    //console.log(req.body.city);
+    let city  = req.body.city;
+    let apikey  = 'a63ca69aa78672a273fa81a6f6a26a95';
+    let url     = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apikey}`;
+    request(url, (err, response, body) => {
+      if (err){
+        console.log(err);
+        res.render('index', {weather: null, error: err });
+      }
+      else {
+        let weather = JSON.parse(body);
+        if(weather.main === undefined){
+          res.render('index', { weather: null, error: 'An error occoured while fetching the Tempreture for this city. Please try again with correct name.' });
+        }
+        else {
+          let weatherText = `The Tempreture in ${ city } is ${ weather.main.temp} degree`;
+          res.render('index', { weather: weatherText, error: null });
+        }
+      }
+    });
+});
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+});
